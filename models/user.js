6eb8c320ad154cedb0c6fca0230fb400bd315d0c
Jari Karri <jari.karri@gmail.com>
2026-06-26 20:46:54 +0300

    fix: poistetaan displayName-validoinnit, koska nimi kopioidaan username-kentästä

--- models/user.js
+++ models/user.js
@@ -20,10 +20,7 @@ const userSchema = new mongoose.Schema(
     // Näyttönimi - näytetään käyttöliittymässä käyttäjän kirjoittamassa muodossa
     displayName: {
       type: String,
-      required: [true, 'Näyttönimi vaaditaan'],
       trim: true,
-      minlength: [3, 'Näyttönimi vähintään 3 merkkiä'],
-      maxlength: [20, 'Näyttönimi enintään 20 merkkiä'],
     },
         // Salasanan hash - alkuperäistä salasanaa ei tallenneta koskaan
     passwordHash: {
